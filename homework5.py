8565484c351cc624925bce6af94560dde83a8f78
Matenk <167009609+Matenk@users.noreply.github.com>
2024-04-25 00:12:15 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['Banana', 'chery', 'apple', 'peach', 'coconut']
+print(my_list)
+print(my_list[:5:4])
+print(my_list[2:5])
+my_list[2] = 'orange'
+print(my_list)
+my_dict = {'Cat' : 'Кошка', 'Dog' : 'Собака', 'Fox' : 'Лиса'}
+print(my_dict)
+print(my_dict['Cat'])
+my_dict['Cat'] = 'Киса'
+my_dict['Fish'] = 'Рыба'
+print(my_dict)
